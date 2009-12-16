5f1730ed47ba4c52b1364738ccf363c571792125
switzer <switzer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-16 16:40:35 +0000

    last of the changes
    git-svn-id: https://plugins.svn.wordpress.org/advertising-manager/trunk@183477 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/Advman/Template/WP2.7/List/Ad.php
@@ -0,0 +1,11 @@
+<?php
+require_once(ADVMAN_TEMPLATE_PATH . '/List.php');
+
+class Advman_Template_List_Ad extends Advman_Template_List
+{
+	function display($ad)
+	{
+		parent::display($ad);
+	}
+}
+?>
